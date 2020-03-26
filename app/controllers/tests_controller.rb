97b9959c73591d05aa6f0a26e86a28a1573501f3
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-03-26 15:36:35 -0400

    updated gemfile

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,12 +2,12 @@ class TestsController < ApplicationController
 
     def index
         tests = Test.all 
-        render json: tests
+        render json: tests.to_json(except: [:created_at, :updated_at])
     end
 
     def show
         test = Test.find(params[:id])
         problems = Problem.all.select {|problem| problem.test_id == test.id }
-        render json: test
+        render json: test.to_json(except: [:created_at, :updated_at])
     end
 end
